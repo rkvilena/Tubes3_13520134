3578993813619f4fd700e8ad71c500359cad69ff
zaydzuhri <zaydzuhri@gmail.com>
2022-04-14 00:52:47 +0700

    added home and test page, elements to be added

--- src/dnaregex/src/app/app-routing.module.ts
+++ src/dnaregex/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'test', component: TestComponent },
+    { path: 'add', component: AddComponent },
+    { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+    
 export class AppRoutingModule { }
--- src/dnaregex/src/app/app.module.ts
+++ src/dnaregex/src/app/app.module.ts
@@ -3,23 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { BackgroundComponent } from './background/background.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    HomeComponent,
+    TestComponent,
+    AddComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
