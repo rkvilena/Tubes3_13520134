8658aef50c6d4ab810d8bc38cc9033a6444bebee
zaydzuhri <zaydzuhri@gmail.com>
2022-04-26 17:35:40 +0700

    started working on search page

--- src/dnaregex/src/app/search/search.component.ts
+++ src/dnaregex/src/app/search/search.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+    searchInput: string = "";
+    searchResults: any[] = [1];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
